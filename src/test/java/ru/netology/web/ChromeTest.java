677ebd344b514ac6e0da355651c0243f90c6fc6f
AAB-87 <andrej-abrekov@yandex.ru>
2021-12-03 10:26:52 +0400

    copied skeleton Driver Management

--- src/test/java/ru/netology/web/ChromeTest.java
+++ src/test/java/ru/netology/web/ChromeTest.java
@@ -1,29 +1,32 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
-public class OrderingCardTest {
+class ChromeTest {
 
     private WebDriver driver;// основная сущность c полем драйвер
 
     @BeforeAll
-    public static void setUpAll() { // метод вызывается 1 раз перед проходом всех тестов
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Acer\\Downloads\\chromedriver_win32"); // указываем что используем и где оно у нас лежит (путь)
+    public static void setUpClass() { // метод вызывается 1 раз перед проходом всех тестов
+        WebDriverManager.chromedriver().setup(); // указываем что используем и где оно у нас лежит (путь)
     }
 
     @BeforeEach // выполняется перед каждым тестом
-    public void SetDriver() { // перед каждым тестом создаём хром драйвер
+    public void setupTest() { // перед каждым тестом создаём хром драйвер
         driver = new ChromeDriver(); // инициализируем поле драйвер новым экземпляром класса
     }
 
     @AfterEach // выполняется после каждого теста
     public void tearDown() { // после каждого теста закрываем браузер и присваиваем null полю driver
-        driver.quit(); // у driver-а вызываем метод выхода для закрытия браузера
-        driver = null;
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 
